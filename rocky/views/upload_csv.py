0490f7a9218250f2761c33cf55fdd1353487bc4e
ammar <ammar.abdulamir@gmail.com>
2022-12-28 15:29:17 +0100

    Some improvements

--- rocky/views/upload_csv.py
+++ rocky/views/upload_csv.py
@@ -1,22 +1,25 @@
-from datetime import datetime, timezone
 import csv
 import io
-from typing import Dict
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.views.generic.edit import FormView
-from django.utils.translation import gettext as _
-from django.urls.base import reverse_lazy
-from django.urls import reverse
-from django.shortcuts import redirect
+from datetime import datetime, timezone
+from typing import Dict, ClassVar, Any
+
 from django.contrib import messages
-from pydantic import ValidationError
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.urls.base import reverse_lazy
+from django.utils.translation import gettext as _
+from django.views.generic.edit import FormView
 from django_otp.decorators import otp_required
-from two_factor.views.utils import class_view_decorator
 from octopoes.api.models import Declaration
-from octopoes.models.ooi.dns.zone import Hostname
-from octopoes.models.ooi.web import URL
-from octopoes.models.ooi.network import Network, IPAddressV4, IPAddressV6
 from octopoes.connector.octopoes import OctopoesAPIConnector
+from octopoes.models import Reference
+from octopoes.models.ooi.dns.zone import Hostname
+from octopoes.models.ooi.network import Network, IPAddressV4, IPAddressV6
+from octopoes.models.ooi.web import URL
+from pydantic import ValidationError
+from two_factor.views.utils import class_view_decorator
+
 from rocky.settings import OCTOPOES_API
 from tools.forms.upload_csv import (
     UploadCSVForm,
@@ -46,18 +49,19 @@ class UploadCSV(PermissionRequiredMixin, FormView):
     form_class = UploadCSVForm
     permission_required = "tools.can_scan_organization"
     success_url = reverse_lazy("ooi_list")
-    reference_cache = {"network": {"internet": Network(name="internet")}}
-    ooi_types = {
-        "hostname": {"type": Hostname},
-        "url": {"type": URL},
-        "network": {"type": Network, "defaultvalue": "internet", "createargument": "name"},
-        "ipaddressv4": {"type": IPAddressV4},
-        "ipaddressv6": {"type": IPAddressV6},
+    reference_cache: Dict[str, Any] = {"Network": {"internet": Network(name="internet")}}
+    ooi_types: ClassVar[Dict[str, Any]] = {
+        "Hostname": {"type": Hostname},
+        "Url": {"type": URL},
+        "Network": {"type": Network, "default": "internet", "argument": "name"},
+        "IPAddressV4": {"type": IPAddressV4},
+        "IPAddressV6": {"type": IPAddressV6},
     }
     skip_properties = ("object_type", "scan_profile", "primary_key")
 
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
+
         self.organization_code = request.user.organizationmember.organization.code
         if not self.organization_code:
             self.add_error_notification(CSV_ERRORS["no_org"])
@@ -71,43 +75,50 @@ class UploadCSV(PermissionRequiredMixin, FormView):
         context["criterias"] = CSV_CRITERIAS
         return context
 
-    def get_or_create_reference(self, ooi_type_name: str, primary_key: str):
-        if ooi_type_name not in self.cache:
-            self.cache[ooi_type_name] = {}
-        if self.cache[ooi_type_name][primary_key]:
-            return self.cache[ooi_type_name][primary_key]
+    def get_or_create_reference(self, ooi_type_name: str, value: str):
+        ooi_type_name = next(filter(lambda x: x.casefold() == ooi_type_name.casefold(), self.ooi_types.keys()))
+
+        self.reference_cache.setdefault(ooi_type_name, {})
+        if value in self.reference_cache[ooi_type_name]:
+            return self.reference_cache[ooi_type_name][value]
+
         ooi_type = self.ooi_types[ooi_type_name]["type"]
-        createarguments = {self.ooi_types[ooi_type_name]["createargument"]: primary_key}
-        referenced_ooi = ooi_type(**createarguments)
-        self.cache[ooi_type_name][primary_key] = referenced_ooi
-        return referenced_ooi
+
+        # set default value if any
+        if value is None:
+            value = self.ooi_types[ooi_type_name].get("default")
+
+        # create the ooi
+        kwargs = {self.ooi_types[ooi_type_name]["argument"]: value}
+        ooi = ooi_type(**kwargs)
+        self.reference_cache[ooi_type_name][value] = ooi
+
+        return ooi
 
     def get_ooi_from_csv(self, ooi_type_name: str, values: Dict[str, str]):
-        ooi_type = self.ooi_types[ooi_type_name].lower()
-        ooi_fields = (
-            (field, URL.__fields__[field].type_ == Reference, URL.__fields__[field].required)
-            for field in URL.__fields__
+        ooi_type = self.ooi_types[ooi_type_name]["type"]
+        ooi_fields = [
+            (field, model_field.type_ == Reference, model_field.required)
+            for field, model_field in ooi_type.__fields__.items()
             if field not in self.skip_properties
-        )
+        ]
         ooi_dict = {}
-        for fieldname, reference, required in ooi_fields:
-            if reference:
+        for field, is_reference, required in ooi_fields:
+            if is_reference and required:
                 try:
-                    referenced_ooi = self.get_or_create_reference(
-                        values.get(fieldname, self.ooi_types[ooi_type_name]["defaultvalue"])
-                    )
+                    referenced_ooi = self.get_or_create_reference2(field, values[field])
                     self._save_ooi(ooi=referenced_ooi, organization=self.organization_code)
-                    ooi_dict[fieldname] = referenced_ooi.reference
+                    ooi_dict[field] = referenced_ooi.reference
                 except IndexError:
                     if required:
                         raise IndexError(
                             "Required referenced primary-key field '%s' not set and no default present for Type '%s'."
-                            % (fieldname, ooi_type_name)
+                            % (field, ooi_type_name)
                         )
                     else:
-                        ooi_dict[fieldname] = None
+                        ooi_dict[field] = None
             else:
-                ooi_dict[fieldname] = values[fieldname]
+                ooi_dict[field] = values.get(field)
         return ooi_type(**ooi_dict)
 
     def _save_ooi(self, ooi, organization) -> None:
@@ -142,9 +153,7 @@ class UploadCSV(PermissionRequiredMixin, FormView):
                 except ValidationError:
                     rows_with_error.append(rownumber + 1)
             if rows_with_error:
-                message = _("Object(s) could not be created for line number(s): ") + ", ".join(
-                    map(str, rows_with_error)
-                )
+                message = _("Object(s) could not be created for row number(s): ") + ", ".join(map(str, rows_with_error))
                 return self.add_error_notification(message)
             self.add_success_notification(_("Object(s) successfully added."))
         except (csv.Error, IndexError):
