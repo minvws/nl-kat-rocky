1b8f477ec5064bbd345b1f4d9da159683635b694
Donny Peeters <donny@bitestreams.com>
2023-01-18 14:15:32 +0100

    Style

--- tests/test_ooi_list_multi_select.py
+++ tests/test_ooi_list_multi_select.py
@@ -8,11 +8,14 @@ def test_ooi_list_delete_multiple(rf, my_user, organization, mock_organization_v
     kwargs = {"organization_code": organization.code}
     url = reverse("ooi_list", kwargs=kwargs)
 
-    request = rf.post(url, data={
-        "ooi": ["Network|internet", "Hostname|internet|scanme.org."],
-        "scan-profile": "L0",
-        "action": "delete",
-    })
+    request = rf.post(
+        url,
+        data={
+            "ooi": ["Network|internet", "Hostname|internet|scanme.org."],
+            "scan-profile": "L0",
+            "action": "delete",
+        },
+    )
     request.resolver_match = resolve(url)
     setup_request(request, my_user)
 
@@ -29,11 +32,14 @@ def test_update_scan_profile_multiple(rf, my_user, organization, mock_organizati
     kwargs = {"organization_code": organization.code}
     url = reverse("ooi_list", kwargs=kwargs)
 
-    request = rf.post(url, data={
-        "ooi": ["Network|internet", "Hostname|internet|scanme.org."],
-        "scan-profile": "L1",
-        "action": "update-scan-profile",
-    })
+    request = rf.post(
+        url,
+        data={
+            "ooi": ["Network|internet", "Hostname|internet|scanme.org."],
+            "scan-profile": "L1",
+            "action": "update-scan-profile",
+        },
+    )
     request.resolver_match = resolve(url)
     setup_request(request, my_user)
     response = OOIListView.as_view()(request, **kwargs)
@@ -46,11 +52,14 @@ def test_update_scan_profile_single(rf, my_user, organization, mock_organization
     kwargs = {"organization_code": organization.code}
     url = reverse("ooi_list", kwargs=kwargs)
 
-    request = rf.post(url, data={
-        "ooi": ["Hostname|internet|scanme.org."],
-        "scan-profile": "L4",
-        "action": "update-scan-profile",
-    })
+    request = rf.post(
+        url,
+        data={
+            "ooi": ["Hostname|internet|scanme.org."],
+            "scan-profile": "L4",
+            "action": "update-scan-profile",
+        },
+    )
     request.resolver_match = resolve(url)
     setup_request(request, my_user)
     response = OOIListView.as_view()(request, **kwargs)
@@ -63,11 +72,14 @@ def test_update_scan_profiles_forbidden_acknowledged(rf, my_user, organization, 
     kwargs = {"organization_code": organization.code}
     url = reverse("ooi_list", kwargs=kwargs)
 
-    request = rf.post(url, data={
-        "ooi": ["Network|internet", "Hostname|internet|scanme.org."],
-        "scan-profile": "L1",
-        "action": "update-scan-profile",
-    })
+    request = rf.post(
+        url,
+        data={
+            "ooi": ["Network|internet", "Hostname|internet|scanme.org."],
+            "scan-profile": "L1",
+            "action": "update-scan-profile",
+        },
+    )
     request.resolver_match = resolve(url)
 
     member = OrganizationMember.objects.get(user=my_user)
@@ -86,11 +98,14 @@ def test_update_scan_profiles_forbidden_trusted(rf, my_user, organization, mock_
     kwargs = {"organization_code": organization.code}
     url = reverse("ooi_list", kwargs=kwargs)
 
-    request = rf.post(url, data={
-        "ooi": ["Network|internet", "Hostname|internet|scanme.org."],
-        "scan-profile": "L1",
-        "action": "update-scan-profile",
-    })
+    request = rf.post(
+        url,
+        data={
+            "ooi": ["Network|internet", "Hostname|internet|scanme.org."],
+            "scan-profile": "L1",
+            "action": "update-scan-profile",
+        },
+    )
     request.resolver_match = resolve(url)
 
     member = OrganizationMember.objects.get(user=my_user)
