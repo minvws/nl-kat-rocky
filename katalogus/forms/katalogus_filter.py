ed68711e4f6865df490efe81ca94351dfa89b207
TwistMeister <roelof.korporaal@gmail.com>
2023-02-07 17:00:19 +0100

    Split filtering and sorting options

--- katalogus/forms/katalogus_filter.py
+++ katalogus/forms/katalogus_filter.py
@@ -6,6 +6,9 @@ from tools.forms.base import BaseRockyForm
 FILTER_OPTIONS = (
     ("enabled", _("Enabled")),
     ("disabled", _("Disabled")),
+)
+
+SORTING_OPTIONS = (
     ("a-z", "A-Z"),
     ("z-a", "Z-A"),
     ("enabled-disabled", _("Enabled-Disabled")),
@@ -17,5 +20,19 @@ class KATalogusFilter(BaseRockyForm):
     """Filter options for plugins listing in KAT-alogus."""
 
     filter_options = forms.ChoiceField(
-        required=False, label=_("Filter options"), choices=FILTER_OPTIONS, widget=forms.RadioSelect()
+        required=False,
+        label=_("Filter options"),
+        choices=FILTER_OPTIONS,
+        widget=forms.RadioSelect()
     )
+
+    sorting_options = forms.ChoiceField(
+        required=False,
+        label=_("Sorting options"),
+        choices=SORTING_OPTIONS,
+        widget=forms.RadioSelect()
+    )
+
+    def __init__(self, *args, **kwargs):
+        print(args, kwargs)
+        super().__init__(*args, **kwargs)
